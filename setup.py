4898bd65ba3adeed16e6c992fcddb4059c5502aa
Raphael Medaer <raphael@medaer.me>
2017-01-31 18:04:47 +0100

    Fixed setup.py linting.

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 
+""" Default setup tools. """
+
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
-    readme = f.read()
+    PROJECT_README = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    PROJECT_LICENSE = f.read()
 
 setup(
     name='sidapi',
     version='0.1.0',
     description='HTTP API to manage configuration repositories from SID project.',
-    long_description=readme,
+    long_description=PROJECT_README,
     author='Raphael Medaer (Escaux)',
     author_email='rme@escaux.com',
     url='',
-    license=license,
+    license=PROJECT_LICENSE,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
