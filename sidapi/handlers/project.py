57816f11c1bf13526daaa652052af2c4230fe320
Raphael Medaer <raphael@medaer.me>
2017-02-07 10:30:01 +0100

    Implemented DELETE method for projects.
    The Project handler can now remove specified project from collection and commit changes.

--- sidapi/handlers/project.py
+++ sidapi/handlers/project.py
@@ -25,4 +25,25 @@ class ProjectHandler(WorkspaceHandler, ErrorHandler, SerializerHandler):
         except RepositoryNotFoundError:
             raise HTTPError(
                 status_code=404,
-                log_message='Project not found.')
+                log_message='Project not found.'
+            )
+
+    def delete(self, name):
+        try:
+            self.pyolite.repos.remove(__projects_prefix__ + name)
+        except RepositoryNotFoundError:
+            raise HTTPError(
+                status_code=404,
+                log_message='Project not found.'
+            )
+
+        try:
+            # Save Gitolite configuration and commit changes
+            self.pyolite.save('Removed project \'%s\'' % name)
+        except IOError:
+            raise HTTPError(
+                status_code=500,
+                log_message='Failed to save changes'
+            )
+
+        self.set_status(204)
